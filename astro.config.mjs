fdf40f389328c305ddb57c581deb86d4b8575f94
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-08-26 14:53:06 -0400

    Added Styling
    - Installed Tailwind and Daisy UI
    - Made basic Button component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // Defined Config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog.app",
   prefetch: true,
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(), tailwind()]
 });
